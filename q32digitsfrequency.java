748917064474da52796d02f52fb6427616cc17cd
Abhinav <125545001+abhinav7677@users.noreply.github.com>
2023-05-21 11:03:50 +0530

    Update q32digitsfrequency.java

--- q32digitsfrequency.java
+++ q32digitsfrequency.java
@@ -12,7 +12,7 @@ public class q32digitsfrequency {
     public static int digitfrequency(int n,int f){
         int rem=0;
         int freq=0;
-        while(n>0){rem=n%10; //
+        while(n>0){rem=n%10; 
         n=n/10;
         if(rem==f){
             freq++;
